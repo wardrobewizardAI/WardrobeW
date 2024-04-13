4434c5ed39f8ddc1be90b48e5cf9fb62a48f93cb
AleSilvaRIvera <alesilvacorreobasura@gmail.com>
2024-04-13 10:21:48 -0400

    first commit

---
+++ react-frontend/src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./firebase/Auth";
+import Login from "./components/Login";
+import SignUpPage from "./components/SignUp";
+import Home from "./components/Home";
+import MyCloset from "./components/MyCloset";
+import OOTD from "./components/OOTD";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase/firebase";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
+function App() {
+  return (
+    <AuthProvider>
+    <Router>
+      <div className="App">
+        <div className="App-body">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/login"
+              element={ <Login />}
+            />
+            <Route
+              path="/register"
+              element={<SignUpPage />}
+            />
+            <Route
+              path="/myCloset"
+              element={<MyCloset />}
+            />
+            <Route
+              path="/ootd"
+              element={<OOTD />}
+            />
+          </Routes>
+        </div>
+        <footer className="App-footer"></footer>
+      </div>
+    </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
---
+++ react-frontend/src/components/StartPage.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import "./StartPage.css";
+import { useNavigate } from 'react-router-dom';
+
+function StartPage() {
+    const navigate = useNavigate()
+  return (
+    <>
+      <div className="start-page-container">
+
+        <div className="left-section">
+          <div className='logo-container'>
+            <img src="/logo.png" alt="logo" />
+          </div>
+          <ul className='navigation text-small-caps'>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+          </ul>
+          <div className='main-content'>
+            <h1>Welcome</h1>
+            <p>
+              Get ready to unlock the full potential of your wardrobe with Wardrobe Wizard!
+              Your stylish companion for effortless wardrobe management!
+            </p>
+
+            <p className='reclaim'>
+              Reclaim your closet!
+            </p>
+            <button className="cta-btn" onClick={()=>navigate("/register")}>Sign Up</button>
+          </div>
+        </div>
+        
+
+        <div className="right-section">
+          <div className='right-side-logo'>
+            <img src="/WWLogo.png" alt="logo" />
+          </div>
+          <div className='start-page-images'>
+            <div className="start-page-image-1">
+              <img src="/StartP1.png" alt="image" />
+            </div>
+            <div className="start-page-image-2">
+              <img src="/StartP2.png" alt="image" />
+            </div>
+            <div className="start-page-image-3">
+              <img src="/StartP3.png" alt="image" />
+            </div>
+          </div>
+        </div>
+
+        <div className='start-page-footer'>
+          <div className='social-media-icons'>
+            <img src="/facebook.png" alt="facebook icon" />
+            <img src="/instagram.png" alt="instagram icon" />
+            <img src="/linkedin.png" alt="linkedin icon" />
+          </div>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default StartPage
